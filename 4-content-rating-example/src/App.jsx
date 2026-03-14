87e61d07b422278903bdd66b505d11479580f465
Dean Brown <deanjbrown221@outlook.com>
2026-03-14 12:09:33 +0000

    Add Content Rating example

---
+++ 4-content-rating-example/src/App.jsx
@@ -0,0 +1,13 @@
+import ContentRating from "./components/ContentRating";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Content Rating Example</h1>
+      <ContentRating />
+    </div>
+  );
+}
+
+export default App;
---
+++ 4-content-rating-example/src/components/ContentRating.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class ContentRating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      // Please note, it is bad practise to have fucntions in state like this
+      handleLike: () => {
+        this.setState((prevState) => ({ likes: prevState.likes + 1 }));
+      },
+
+      handleDislike: () => {
+        this.setState((prevState) => ({ dislikes: prevState.dislikes + 1 }));
+      },
+    };
+  }
+  render() {
+    return (
+      <div className="contentRating">
+        <p>Some content to rate</p>
+        <div className="ratingButtons">
+          <button className="likeButton" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislikeButton" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContentRating;
